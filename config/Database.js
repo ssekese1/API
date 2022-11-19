44628a513a5be56e59d14035952361c94400f62b
ssekese1 <senaki.sekese@gmail.com>
2022-11-19 23:39:20 +0200

    add config folder

---
+++ config/Database.js
@@ -0,0 +1,18 @@
+import { Sequelize } from "sequelize";
+import dbConfig from "./dbconfig.js";
+
+// Open the connection to MySQL server
+
+
+const db = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    pool: {
+        max: dbConfig.pool.max,
+        min: dbConfig.pool.min,
+        acquire: dbConfig.pool.acquire,
+        idle: dbConfig.pool.idle
+      }
+});
+
+export default db;
